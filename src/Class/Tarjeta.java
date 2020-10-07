7c23bb853e0cb234d8cc33d40b52d534d9180c43
Isaca1996 <isaacedmundo.hernandez@gmail.com>
2020-10-07 20:45:46 -0300

    Subiendo caja y sus adheridos

---
+++ src/Class/Tarjeta.java
@@ -0,0 +1,19 @@
+package Class;
+
+
+public class Tarjeta {
+	
+	public String nombre; 
+	public String apellido;
+	public String tipo;
+	public int numero;
+	public int fechaDeVcto;
+	public int codigoSeg;
+	public double saldo;
+	
+	public void pasarTarjeta() {
+		
+	}
+	
+	
+}
---
+++ src/Class/Ticket.java
@@ -0,0 +1,17 @@
+package Class;
+
+
+public class Ticket {
+	
+	public String tipoDeTarjeta;
+	public String nombreComprador;
+	public int serial;
+	public int fecha;
+	public double saldo;
+	public boolean firmado;
+	
+	public void firmar() {
+		
+	}
+	
+}
